d85954e980f563218b3c16e7ab4574e0e11b1fd2
unknown <mariakyriacou07@outlook.com>
2022-11-27 22:23:42 +0200

    My python files

---
+++ strings.py
@@ -0,0 +1,8 @@
+print('"Is it very long?" Alice asked,for she had heard a good deal of poetry that day\n\n"It\'s long", said the Knight,"but very,very beautiful "')
+print("""
+    "Is it very long?"Alice asked,for she had heard a good deal of poetry that day.
+    "It's long", said the Knight,"but very,very beautiful 
+     """)
+print(""""Is it very long?" Alice asked, for she had heard a good deal of poetry that day.
+
+"It's long," said the Knight, "but it's very, very beautiful."""")
