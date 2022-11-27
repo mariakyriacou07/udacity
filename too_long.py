138dd7f15b162959c0e1a1925f3e4f993ceba568
unknown <mariakyriacou07@outlook.com>
2022-11-27 22:33:30 +0200

    Modified file

--- too_long.py
+++ too_long.py
@@ -11,3 +11,4 @@ print(too_long("I'm a short string!"))
 # Test a long string
 print("This should be True:")
 print(too_long("Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal."))
+print("Hi")
